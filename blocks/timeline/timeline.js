a3eed958eb9ec4dc88b247a849d72622835e4a25
milan <jevga.milan@gmail.com>
2025-08-06 10:24:40 +0200

    searches for table elements as divs and not tr elemtns

--- blocks/timeline/timeline.js
+++ blocks/timeline/timeline.js
@@ -1,13 +1,16 @@
 export default function decorate(block) {
-  const rows = [...block.querySelectorAll('tr')];
   const wrapper = document.createElement('div');
   wrapper.className = 'timeline-wrapper';
 
-  rows.forEach((row) => {
-    const cols = row.querySelectorAll('td');
+  [...block.children].forEach((row) => {
+    const cols = row.children;
+    if (cols.length !== 2) return; // Skip malformed rows
+
     const imageCell = cols[0].querySelector('img') ? cols[0] : cols[1];
     const textCell = cols[0].querySelector('img') ? cols[1] : cols[0];
     const image = imageCell.querySelector('img');
+
+    if (!image) return;
 
     const item = document.createElement('div');
     item.className = `timeline-item ${imageCell === cols[0] ? 'left' : 'right'}`;
